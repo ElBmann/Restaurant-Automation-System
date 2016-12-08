f5c6158c348c097d513ef100f98d8490da495829
ElBmann <brianrecuero@gmail.com>
2016-12-07 20:29:44 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -7,53 +7,53 @@ package Final_Project;
 
 import java.sql.*;
 public class Driver {
-	public static String url="jdbc:mysql://localhost:3306/ras";//the Url name localhost:3306 is deafult for sql workbench.
+	public static String url="jdbc:mysql://localhost:3306/ras?autoReconnect=true&useSSL=false";//the Url name localhost:3306 is deafult for sql workbench.
 	public static String user = "root";//User Name
 	public static String pass = "Cosmo01392";//Password
 	public static Connection myConn;
-	
+
 	public static Connection getConnection() {
-		
+
 		try{
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			 myConn=DriverManager.getConnection(url,user,pass);//..Connecton to data base
-			
-			
+			myConn=DriverManager.getConnection(url,user,pass);//..Connecton to data base
+
+
 		}
 		catch(Exception exc){
 			exc.printStackTrace();
 		}
 		return myConn;
-			
-			
-			
-			
-			
-			
-			
-			
-			//ResultSet myRs = myStmt.executeQuery("select * from Orders");//...Execute SQL query Table name is employee
-			
-			//while(myRs.next()){
-				//System.out.println((("\n\n Table Number: ")+myRs.getInt("Table_Num")+("\n Order Number: ")+myRs.getInt("Order_Num")+("\n Items: ")+ myRs.getString("Item_list")));//shows what is in the data base
-			//}
-			
-			//String sql ="delete from orders where Order_Num='127'"; 
-					
-			//int rowsAffected = myStmt.executeUpdate(sql);
-					
-			//myStmt.executeUpdate(sql);
-			
-			//System.out.println("Rows affected: "+rowsAffected);
-			//System.out.println("Delete complete");
-			
-			
-			//ResultSet myRs = myStmt.executeQuery( "SELECT distinct "+jTextField.getText()+
-	   //// " as call from tablename");
-	   // List<String> results = new ArrayList<String>();
-	    //while(rs.next()) {
-	       // results.add(rs.getString(1));
-	  /**  }
+
+
+
+
+
+
+
+
+		//ResultSet myRs = myStmt.executeQuery("select * from Orders");//...Execute SQL query Table name is employee
+
+		//while(myRs.next()){
+		//System.out.println((("\n\n Table Number: ")+myRs.getInt("Table_Num")+("\n Order Number: ")+myRs.getInt("Order_Num")+("\n Items: ")+ myRs.getString("Item_list")));//shows what is in the data base
+		//}
+
+		//String sql ="delete from orders where Order_Num='127'"; 
+
+		//int rowsAffected = myStmt.executeUpdate(sql);
+
+		//myStmt.executeUpdate(sql);
+
+		//System.out.println("Rows affected: "+rowsAffected);
+		//System.out.println("Delete complete");
+
+
+		//ResultSet myRs = myStmt.executeQuery( "SELECT distinct "+jTextField.getText()+
+		//// " as call from tablename");
+		// List<String> results = new ArrayList<String>();
+		//while(rs.next()) {
+		// results.add(rs.getString(1));
+		/**  }
 			 while (myRs.next())
              {
 				 List rowValues = new ArrayList();
@@ -61,9 +61,9 @@ public class Driver {
                  contactListNames[1] = myRs.getString(1);
                  System.out.println("" + contactListNames[1]);
 		}
-		*/
-		
-	
+		 */
+
+
 	}
 }
 
