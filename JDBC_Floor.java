14f8a3d8f4743c8e50eec2f5361163d48cb793ec
ElBmann <brianrecuero@gmail.com>
2016-12-11 16:42:20 -0500

    Add files via upload

---
+++ JDBC_Floor.java
@@ -0,0 +1,114 @@
+package Final_Project;
+import java.sql.*;
+import Final_Project.Driver;
+
+public class JDBC_Floor 
+{
+	public Statement myStat;
+	public ResultSet myRs;
+	public int intmyRs;
+	public int exc;
+	
+	Connection conn = Driver.getConnection();
+	
+	public boolean addTable(int number, int size)
+	{
+		String status = "Ready";
+		int numCheck = searchFloor(number);
+		if(numCheck==-1)
+		{
+			try{	
+				//1: Create a statement 
+				myStat = conn.createStatement();
+				//2: Execute SQL
+				exc = myStat.executeUpdate("INSERT INTO RAS_Table (Table_Num,Table_Size,Table_Status) VALUES ("+number+","+size+",'"+status+"');");           
+			}
+			//catch any exception
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		return true;
+		}
+		else
+			return false;
+	}
+	
+	public Table removeTable(int number)
+	{
+		int numCheck = searchFloor(number);
+		if(numCheck==-1) //Table does not exist
+		{
+			return null;
+		}
+		else //Table does exist
+		{
+			try
+			{
+				int num=0;
+				int size=0;
+				//1: Create a statement 
+				myStat = conn.createStatement();
+				//2: Execute SQL
+				myRs = myStat.executeQuery("select * from RAS_Table WHERE Table_Num = "+number+";");
+				//3: Process the result set
+				while (myRs.next())
+				{
+					num=myRs.getInt("Table_Num"); //Retrieve Table Number from database
+					size=myRs.getInt("Table_Size"); //Retrieve Table Size from database
+				}	
+				PreparedStatement st = conn.prepareStatement("DELETE FROM RAS_Table WHERE Table_Num = '" +number+ "';");
+				st.executeUpdate();
+				Table t = new Table(num,size);
+				return t; //return the removed Table
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
+	
+	public int searchFloor(int number)
+	{
+		try
+		{
+			int check = -1; //represents Table Number
+			//1: Create a statement 
+			myStat = conn.createStatement();
+			//2: Execute SQL
+			myRs = myStat.executeQuery("select * from RAS_Table WHERE Table_Num = "+number+";");
+			//3: Process the result set
+			while (myRs.next())
+			{
+				check = myRs.getInt("Table_Num"); //set to the Table Number found in database
+			}	
+			return check; //return the found Table Number
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return -1; //Table Number not found
+	}
+	
+	public String toString()
+	{
+		String r = "";
+		try
+		{
+			//1: Create a statement 
+			myStat = conn.createStatement();
+			//2: Execute SQL
+			myRs = myStat.executeQuery("select * from RAS_Table");
+			//3: Process the result set
+			while (myRs.next())
+			{
+				r+=("Table #"+myRs.getInt("Table_Num")+"  Size="+myRs.getInt("Table_Size")+"  Status="+myRs.getString("Table_Status")+"\n");
+			}	
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return r;
+	}
+
+}
