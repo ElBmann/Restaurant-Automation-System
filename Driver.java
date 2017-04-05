7fd17e4447d7104d366d0e48ca1f107cedfecd95
Brian Recuero <brianrecuero@gmail.com>
2017-04-05 11:57:23 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,69 +1,27 @@
-package Final_Project;
-//Make sure you have all the SQL Stuff get SQL installer it will download everthing you need DAWG!
-//next you need to name your sql database in this instances its called "sample"
-//localhost 3306 seems to be the default name of the url
-//when setting up the work bench it will ask you to make a user and pass
-
-
+package RAS;
 import java.sql.*;
+
 public class Driver {
-	public static String url="jdbc:mysql://localhost:3306/ras?autoReconnect=true&useSSL=false";//the Url name localhost:3306 is deafult for sql workbench.
-	public static String user = "root";//User Name
-	public static String pass = "Cosmo01392";//Password
+	
 	public static Connection myConn;
-
+	public static String url = "jdbc:mysql://localhost:3306/RAS?autoReconnect=true&useSSL=false";
+	public static String username = "root";
+	public static String password = "gokuisssj10"; //enter password here
+	
+	
 	public static Connection getConnection() {
-
+		
 		try{
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			myConn=DriverManager.getConnection(url,user,pass);//..Connecton to data base
-
-
+			
+			//1: Get Connection to DB
+			myConn = DriverManager.getConnection(url,username,password);
+			//myStat = myConn.createStatement();
 		}
-		catch(Exception exc){
-			exc.printStackTrace();
+		catch (Exception e){
+			e.printStackTrace();
 		}
 		return myConn;
-
-
-
-
-
-
-
-
-		//ResultSet myRs = myStmt.executeQuery("select * from Orders");//...Execute SQL query Table name is employee
-
-		//while(myRs.next()){
-		//System.out.println((("\n\n Table Number: ")+myRs.getInt("Table_Num")+("\n Order Number: ")+myRs.getInt("Order_Num")+("\n Items: ")+ myRs.getString("Item_list")));//shows what is in the data base
-		//}
-
-		//String sql ="delete from orders where Order_Num='127'"; 
-
-		//int rowsAffected = myStmt.executeUpdate(sql);
-
-		//myStmt.executeUpdate(sql);
-
-		//System.out.println("Rows affected: "+rowsAffected);
-		//System.out.println("Delete complete");
-
-
-		//ResultSet myRs = myStmt.executeQuery( "SELECT distinct "+jTextField.getText()+
-		//// " as call from tablename");
-		// List<String> results = new ArrayList<String>();
-		//while(rs.next()) {
-		// results.add(rs.getString(1));
-		/**  }
-			 while (myRs.next())
-             {
-				 List rowValues = new ArrayList();
-             }
-                 contactListNames[1] = myRs.getString(1);
-                 System.out.println("" + contactListNames[1]);
-		}
-		 */
-
-
 	}
-}
 
+}
